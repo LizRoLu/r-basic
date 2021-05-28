b076bcb2975cceb5173d60a4f3d14ab7ef2029f8
LizRoLu <58675048+LizRoLu@users.noreply.github.com>
2021-05-27 23:59:53 -0500

    Ejercicios
    Ejercicios en curso

---
+++ Mis ejercicios/Scripts/02. Install.R
@@ -0,0 +1,39 @@
+install.packages("tidyverse", dep = TRUE)
+
+library(tidyverse)
+
+install.packages("magic", dep = TRUE)
+
+library(magic)
+
+magic(6)
+
+installed.packages()
+
+#También se pueden instalar los paquetes desde la pestaña "Packages"
+#de la ventana inferior izquierda.
+#Con cierta frecuencia es recomendable hacer actualizaciones.
+
+choose(11, 3)
+
+
+
+# Creació de variables y funciones
+
+x = (pi^2)/2
+x
+
+y <- cos(pi/4)
+y
+
+sin(pi/4) + cos(pi/4) -> z
+z
+
+edad <- 38
+nombre = "Lizbeth"
+
+pi.4 = pi*4
+pi.4
+
+
+
